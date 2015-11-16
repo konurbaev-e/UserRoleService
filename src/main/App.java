1dfff828cd749caeb8266ea34fc1185c4649b4dd
konurbaev-e <Konurbaev-E@gaz-is.ru>
2015-11-16 19:15:37 +0300

    first commit

---
+++ src/main/App.java
@@ -0,0 +1,20 @@
+package main;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
